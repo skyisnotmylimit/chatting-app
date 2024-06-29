d81a1fc6e4b48f86ca66a07d877f215469912599
ShubhamGupta9453 <shubhamgupta9454666551@gmail.com>
2024-06-29 18:36:55 +0530

    first commit

---
+++ frontend/src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./myStyles.css";
+function Sidebar() {
+  return (
+    <div className='sidebar-container'>
+      SideBar
+    </div>
+  )
+}
+
+export default Sidebar
